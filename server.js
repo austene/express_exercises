5406a9d003caf9e82a874ec287ea9e726c658307
AE <austence@sbcglobal.net>
2021-10-09 15:13:57 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+
+const requireJsonContent = () => {
+  return (req, res, next) => {
+    if(req.headers['content-type'] !== 'application/json') {
+      res.status(400).send('Server requires application/json')
+    } else {
+      next()
+    }
+  }
+};
+
+// //log all requests
+// app.use((req, res, next) => {
+//   console.log(req)
+//   next();
+// })
+
+app.get('/', (req, res, next) => {
+  res.send('Welcome Home');
+});
+
+app.post('/', requireJsonContent(), (req, res, next) => {
+  res.send('You sent JSON');
+});
+
+app.listen(3000);
